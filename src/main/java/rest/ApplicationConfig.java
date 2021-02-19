d5e16ac920abed6371bcad6f6b3e7a670a2b0668
Emil Elkjær Nielsen <emil@visiontrade.dk>
2021-02-19 11:00:52 +0100

    Add CorsFilter

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -18,6 +18,7 @@ public class ApplicationConfig extends Application {
    * defined in the project. If required, comment out calling this method in getClasses().
    */
   private void addRestResourceClasses(Set<Class<?>> resources) {
+    resources.add(cors.CorsFilter.class);
     resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
     resources.add(rest.MovieResource.class);
   }
